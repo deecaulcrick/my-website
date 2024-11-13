02c51423f28a4242b3f394be6750e090215fbb1f
Dee Caulcrick <deecaulcrick@gmail.com>
2024-11-13 08:51:14 +0100

    initial commit

---
+++ articles.js
@@ -0,0 +1,55 @@
+const projects = [
+  {
+    id: "01",
+    title:
+      "The TCP/IP and OSI Models — What happens when you send that risky text?",
+    tag: "Networks",
+    link: "https://deecaulcrick.com/posts/what-happens-when-you-send-a-risky-text",
+    image: "yearning.png",
+  },
+  {
+    id: "02",
+    title: "What the heck is an IP Address?",
+    tag: "Networks",
+    link: "https://deecaulcrick.com/posts/what-is-an-ip-address",
+    image: "ip-address.png",
+  },
+  {
+    id: "03",
+    title:
+      "A Lazy Computer Science Student's Guide To Building An Adding Machine",
+    tag: "Digital Logic",
+    link: "https://deecaulcrick.com/posts/how-to-build-an-adding-machine",
+    image: "13.png",
+  },
+  {
+    id: "04",
+    title:
+      "Transforming The Creator Economy Through Decentralization with Web5",
+    tag: "Web5",
+    link: "https://deecaulcrick.hashnode.dev/transforming-the-creator-economy-through-decentralization-with-web5",
+    image: "web5.avif",
+  },
+  {
+    id: "05",
+    title: "An Introduction to Web Scraping using Node.js and Cheerio",
+    tag: "Web Scraping",
+    link: "https://deecaulcrick.com/posts/web-scraping-with-nodejs-and-cheerio",
+    image: "web-scraping.png",
+  },
+  {
+    id: "06",
+    title:
+      "Messy Desks and Brilliant Minds - Embracing the Chaos of Creativity",
+    tag: "Blog",
+    link: "https://deecaulcrick.com/posts/messy-desks-brilliant-minds",
+    image: "chaos.gif",
+  },
+  {
+    id: "07",
+    title: "Understanding Bubble Sort Algorithm",
+    tag: "Algorithms",
+    link: "https://deecaulcrick.com/snippets/bubble-sort",
+    image: "1.png",
+  },
+];
---
+++ script.js
@@ -0,0 +1,45 @@
+// app.js
+document.addEventListener("DOMContentLoaded", () => {
+  const projectsList = document.getElementById("projects-list");
+
+  projects.forEach((project) => {
+    const projectDiv = document.createElement("a");
+    projectDiv.href = project.link;
+    projectDiv.target = "_blank";
+    projectDiv.className = "project";
+
+    projectDiv.innerHTML = `
+        <h4><sup>(${project.id})</sup> ${project.title}</h4>
+        <img width="100%" class="project__image" src="images/${project.image}" alt="${project.title}" />
+        <h5>${project.tag}</h5>
+        <div class="arrowBox">
+          <img src="/images/Arrow.svg" alt="Arrow" width="20px" />
+        </div>
+      `;
+
+    projectsList.appendChild(projectDiv);
+  });
+});
+
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  console.log(firstElement);
+  parentSelector.insertAdjacentHTML("beforeend", clone);
+  parentSelector.insertAdjacentHTML("beforeend", clone);
+
+  setInterval(function () {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+
+//after window is completed load
+//1 class selector for marquee
+//2 marquee speed 0.2
+window.addEventListener("load", Marquee(".marquee", 0.2));
